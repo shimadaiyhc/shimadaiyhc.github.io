1ce229f44ba2ac6c4a398c5881d4bac28963edcf
yukuduri <duriansmile3960@gmail.com>
2018-03-17 03:14:19 +0900

    footer要素、部員専用ページの一部修正

--- js/memberPageTop.js
+++ js/memberPageTop.js
@@ -13,12 +13,15 @@ if(document.referrer.length !=0){
 //URLから直接アクセスされた時
 }else{
     document.write("ダイレクトアクセスを確認");　//デバッグ用
+    //  /*
     directAccessError();
     function directAccessError(){
         location.href="../error/directAccessError.html";
     }
+    //  */
 }
 document.write("<h1>島大ユース部員専用ページへようこそ！</h1>",
                "<h2>このページは準備中です(._.`)ｺﾞﾒﾝﾈ</h2>",
-               "<p>こちらのページには、部則などを載せようかと考えています</p>",
+               "<p>こちらのページには、部則などを載せようかと考えています。<br>尚、こちらのページは、情報漏えい防止の都合上、JavaScriptで記述されています。<br>コーディングしてる中の人の技術が上がるまで、見た目がシンプルなままだと思います。</p>",
                "<h3>© 2017 ShimadaiY.H.C.</h3>");
+document.close();
