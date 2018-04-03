ffa51690541b0664db7573e51c8d9c4d3ebef748
yukuduri <duriansmile3960@gmail.com>
2018-04-03 23:46:49 +0900

    インデントの調整、キャンセル時のアラート表示を削除

--- js/TopPage.js
+++ js/TopPage.js
@@ -1,26 +1,20 @@
 function enter(){
-    pass = prompt("パスワードを入力してください","");
-    if(pass != "" && pass != null){
-        location.href = pass + '/';
-    }
+  pass = prompt("パスワードを入力してください","");
+  if(pass != "" && pass != null){
+    location.href = pass + '/';
+  }
 }
 
 function toOldPages(){
-        if(confirm("旧サイトへジャンプしますか？\n※スマホからはアクセスできません。") == true){
-        location.href = "http://shimadaiyouth.bufsiz.jp/";
-    }else{
-        alert("キャンセルされました");
-    }
+  if(confirm("旧サイトへジャンプしますか？\n※スマホからはアクセスできません。") == true){
+    location.href = "http://shimadaiyouth.bufsiz.jp/";
+  }
 }
 
 function viewReason(){
-        if(confirm("このサイトが作られた理由を見ますか？") == true){
-        if(confirm("本当に見ますか？")==true){
-            location.href = "../about.html";
-        }else{
-            alert("キャンセルされました");
-        }
-    }else{
-        alert("キャンセルされました");
-    }
+  if(confirm("このサイトが作られた理由を見ますか？") == true){
+    if(confirm("本当に見ますか？")==true){
+      location.href = "../about.html";
+     }
+  }
 }
